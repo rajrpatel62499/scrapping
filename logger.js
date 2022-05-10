eabb36f3369b3e8ceb61f05ac91071a126758953
Raj Patel <rajrpatel62499@gmail.com>
2022-05-10 19:19:51 -0400

    questions and answer croping added

---
+++ logger.js
@@ -0,0 +1,20 @@
+const { createLogger, format, transports } = require("winston");
+ 
+const logLevels = {
+  fatal: 0,
+  error: 1,
+  warn: 2,
+  info: 3,
+  debug: 4,
+  trace: 5,
+};
+ 
+const logger = createLogger({
+  levels: logLevels,
+  format: format.combine(format.timestamp(), format.json()),
+  transports: [new transports.File({ filename: 'file.log'})],
+  exceptionHandlers: [new transports.File({ filename: "exceptions.log" })],
+  rejectionHandlers: [new transports.File({ filename: "rejections.log" })],
+});
+
+module.exports = logger ;
