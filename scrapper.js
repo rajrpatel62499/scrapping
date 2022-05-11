34cc0c18071f06bb5a61c26b669b85310e10dc8a
Raj Patel <rajrpatel62499@gmail.com>
2022-05-11 17:58:30 -0400

    page vise error handling added

--- scrapper.js
+++ scrapper.js
@@ -40,73 +40,77 @@ const extractFolderNameFromUrl = (url) => {
         await page.setViewport({ height: 1080, width: 1920 })
         
         for (let urlIndex = 0; urlIndex < urls.length; urlIndex++) {
-            const url = urls[urlIndex];
-            console.log(`WORKING ON URL: ${url} `);
-            await page.goto(url);
-        
-            const folderName = extractFolderNameFromUrl(url);
-        
-            const getQuestionCount = async () => {
-                const innerText = await page.$eval("#question", el => {return el.lastElementChild.lastElementChild.innerHTML});
-                const count = Number.parseInt(innerText.split(":")[1]);
-                return count;        
-            }
-        
-            const totalQuestions = await getQuestionCount();
-            console.log(`Total Questions: ${totalQuestions}`);
-        
-            for (let queIndex = 0; queIndex < totalQuestions; queIndex++) {
-                console.log(`Fetching question ${queIndex+1} images`);
-                await page.goto(url + `${queIndex+1}`);
-                
-                const dir = `./images/${folderName}/${queIndex+1}`;
-                if (storeImage) {
-                    makeDirIfNotExist(dir);
-                }
-
-                const cropQuestion = async () => {
-                    const questionDiv = await page.$("#imagewrap");
-                    let file;
-                    if (storeImage) {
-                        file = await questionDiv.screenshot({ path: `${dir}/question.png` });
-                    } else {
-                        file = await questionDiv.screenshot();
-                    }
-                    const base64 = Buffer.from(file).toString('base64') 
-                    const data = await convertImageToMathml(base64);
-                    if (data && data?.data?.html) {
-                        const mathml = data.data.html;
-                    }
+            try {
+                const url = urls[urlIndex];
+                console.log(`WORKING ON URL: ${url} `);
+                await page.goto(url);
+            
+                const folderName = extractFolderNameFromUrl(url);
+            
+                const getQuestionCount = async () => {
+                    const innerText = await page.$eval("#question", el => {return el.lastElementChild.lastElementChild.innerHTML});
+                    const count = Number.parseInt(innerText.split(":")[1]);
+                    return count;        
                 }
             
-                const cropAnswers = async () => {
-                    try {
-                        const answers = await page.$$("#answers > .list-group-item");
-
-                        for (let answerIndex = 0; answerIndex < answers.length; answerIndex++) {
-                            const answer = answers[answerIndex];
-                            await page.waitForTimeout(1000);
-
-                            if (storeImage) {
-                                file = await answer.screenshot({ path: `${dir}/option-${answerIndex + 1}.png` });
-                            } else {
-                                file = await answer.screenshot();
-                            }
-                            const base64 = Buffer.from(file).toString('base64') 
-                            const data = await convertImageToMathml(base64);
-                            if (data && data?.data?.html) {
-                                const mathml = data.data.html;
-                            }
-                        }
-
-                    } catch (error) {
-                        console.error(error);                        
+                const totalQuestions = await getQuestionCount();
+                console.log(`Total Questions: ${totalQuestions}`);
+            
+                for (let queIndex = 0; queIndex < totalQuestions; queIndex++) {
+                    console.log(`Fetching question ${queIndex+1} images`);
+                    await page.goto(url + `${queIndex+1}`);
+                    
+                    const dir = `./images/${folderName}/${queIndex+1}`;
+                    if (storeImage) {
+                        makeDirIfNotExist(dir);
                     }
+    
+                    const cropQuestion = async () => {
+                        const questionDiv = await page.$("#imagewrap");
+                        let file;
+                        if (storeImage) {
+                            file = await questionDiv.screenshot({ path: `${dir}/question.png` });
+                        } else {
+                            file = await questionDiv.screenshot();
+                        }
+                        const base64 = Buffer.from(file).toString('base64') 
+                        const data = await convertImageToMathml(base64);
+                        if (data && data?.data?.html) {
+                            const mathml = data.data.html;
+                        }
+                    }
+                
+                    const cropAnswers = async () => {
+                        try {
+                            const answers = await page.$$("#answers > .list-group-item");
+    
+                            for (let answerIndex = 0; answerIndex < answers.length; answerIndex++) {
+                                const answer = answers[answerIndex];
+                                await page.waitForTimeout(1000);
+    
+                                if (storeImage) {
+                                    file = await answer.screenshot({ path: `${dir}/option-${answerIndex + 1}.png` });
+                                } else {
+                                    file = await answer.screenshot();
+                                }
+                                const base64 = Buffer.from(file).toString('base64') 
+                                const data = await convertImageToMathml(base64);
+                                if (data && data?.data?.html) {
+                                    const mathml = data.data.html;
+                                }
+                            }
+    
+                        } catch (error) {
+                            console.error(error);                        
+                        }
+                    }
+            
+                    await cropQuestion();
+                    await cropAnswers();
+                    console.log(`Done cropping Question ${queIndex+1} `);
                 }
-        
-                await cropQuestion();
-                await cropAnswers();
-                console.log(`Done cropping Question ${queIndex+1} `);
+            } catch (error) {
+                urlIndex++;
             }
             
         }
